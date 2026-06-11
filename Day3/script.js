4a69544e374e015294f4b930f140a88e924780c6
sarva1210 <sarva.vish77@gmail.com>
2026-06-11 20:22:25 +0530

    conditional

--- Day3/script.js
+++ Day3/script.js
@@ -4,3 +4,17 @@ if(a>10){
 } else{
     console.log("bye bye!");
 }
+
+
+if(10){
+    console.log("hey");
+} else{
+    console.log("goodbye!");
+} //truthy
+
+
+if(0){
+    console.log("hie");
+}else{
+    console.log("byee");
+} //falsy 
