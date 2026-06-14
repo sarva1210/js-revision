f8139070965bbf5d641f17376c6f4a931e3750f4
sarva1210 <sarva.vish77@gmail.com>
2026-06-14 13:07:49 +0530

    basic js que

--- Day5/script.js
+++ Day5/script.js
@@ -7,3 +7,12 @@ for (let i = 1; i < 11; i++) {
 for (let i = 1; i < 11; i++) {
     console.log(`38 x ${i} = ${38*i}`);
 }
+
+// how many num b/w 1 to 15 greater than 8 loop and count conditionally
+let count = 0;
+for (let i = 0; i < 16; i++) {
+    if (i>8) {
+        count++
+    }
+}
+console.log(`total count is ${count}`);
