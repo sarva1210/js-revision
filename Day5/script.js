bfed04c5fa46fd50b4845b2f2ac08eadb0b7246a
sarva1210 <sarva.vish77@gmail.com>
2026-06-14 13:07:05 +0530

    basic js que

---
+++ Day5/script.js
@@ -0,0 +1,9 @@
+// print the multiplication table of 5
+for (let i = 1; i < 11; i++) {
+    console.log(`5 x ${i} = ${5*i}`);
+}
+
+// same for 38
+for (let i = 1; i < 11; i++) {
+    console.log(`38 x ${i} = ${38*i}`);
+}
