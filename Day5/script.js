fec1373f6717424b8cf38063de59529ad60308e9
sarva1210 <sarva.vish77@gmail.com>
2026-06-14 13:09:42 +0530

    basic js que

--- Day5/script.js
+++ Day5/script.js
@@ -8,6 +8,7 @@ for (let i = 1; i < 11; i++) {
     console.log(`38 x ${i} = ${38*i}`);
 }
 
+
 // how many num b/w 1 to 15 greater than 8 loop and count conditionally
 let count = 0;
 for (let i = 0; i < 16; i++) {
@@ -16,3 +17,17 @@ for (let i = 0; i < 16; i++) {
     }
 }
 console.log(`total count is ${count}`);
+
+
+// ask user for password and print access status hardcoded correct password . Compare with user input
+let password = "123456";
+let pass = prompt("tell your password");
+if (pass === null) {
+    console.error("you cancelled it....");
+}else{
+    if (pass===password) {
+        console.log("Matched!");
+    }else{ 
+        console.log("Not matched");
+    }
+}
