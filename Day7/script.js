dfd212128b7b5deb351f73305fd46e616554041d
sarva1210 <sarva.vish77@gmail.com>
2026-06-16 20:38:32 +0530

    basic js que

--- Day7/script.js
+++ Day7/script.js
@@ -47,3 +47,22 @@ function values(a,b,c) {
     console.log(a,b,c);
 }
 values(1,2) //default
+
+
+function cd(a,b,c,d) {
+    console.log(a,b,c,d);
+}//positional
+cd(1,2,3,4); //value dena zaruri hai (1,2,null,4) if c nhi dena toh kuch value dena padega
+
+
+function ef(a,b=3,c,d) {
+    console.log(a,b,c,d);
+}//default
+ef(1,undefined,3,4);
+
+
+function gh(a,b,c,d) {
+    console.log(a,b,c,d);
+}//spread
+let arr = [5,6,7,8];
+gh(...arr);
