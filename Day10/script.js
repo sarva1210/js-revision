780baf401da7073f1385b084bad40006da804ea7
sarva1210 <sarva.vish77@gmail.com>
2026-06-19 19:43:33 +0530

    pure , impure

--- Day10/script.js
+++ Day10/script.js
@@ -6,3 +6,20 @@ function runTwice(fn) {
 runTwice(function(){
     console.log("hello")
 });
+
+
+// Create one pure function that always returns the same output for a given input,and one impure function using a global variable.
+function pure(a,b) {
+    console.log(a+b);
+}
+pure(1,2)
+pure(1,2)
+
+let global=0;
+function impure(c) {
+    global++;
+    console.log(c+global);
+}
+
+impure(4) //5
+impure(4) //6
