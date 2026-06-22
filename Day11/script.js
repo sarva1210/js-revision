2c359ace13623c19ac9584574a6979542d97a7bd
sarva1210 <sarva.vish77@gmail.com>
2026-06-22 23:39:38 +0530

    array

--- Day11/script.js
+++ Day11/script.js
@@ -6,3 +6,23 @@ for(let num of arr){
         max = num;
 }
 console.log(max);
+
+
+// Count how many numbers in an array are greater than 60.
+let arr2 = [20,50,56,79,80];
+let count = 0;
+for(let num of arr2){
+    if(num>60)
+        count++;
+}
+console.log(count);
+
+
+// Reverse an array manually using a loop (without .reverse()).
+let arr3 = [2, 4, 6, 8, 10];
+let rev = [];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    rev.push(arr[i]);
+}
+console.log(rev);
