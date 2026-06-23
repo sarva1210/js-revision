b46e1af5485e483d07bc12a4e84d14eae6553fce
sarva1210 <sarva.vish77@gmail.com>
2026-06-23 20:44:33 +0530

    obj

--- Day12/script.js
+++ Day12/script.js
@@ -1,1 +1,11 @@
 // oops in js
+
+// Simple Object Example
+const user = {
+    name: "John",
+    age: 18,
+    greet(){
+        console.log(`hello my name is ${this.name} and my age is ${this.age}`)
+    }
+}
+user.greet();
