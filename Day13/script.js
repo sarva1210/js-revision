77e87a6b164357c531b0f36fc71aed5268abfdd3
sarva1210 <sarva.vish77@gmail.com>
2026-06-24 22:04:33 +0530

    apply / bind

--- Day13/script.js
+++ Day13/script.js
@@ -10,6 +10,7 @@ const obj = {
 obj.show();
 
 
+// `call()`
 // Calls a function with a specific `this` value.
 // The `call()` method allows you to invoke a function and explicitly set the value of `this` within that function. 
 // It takes the first argument as the value to be used as `this`, followed by any additional arguments that the function may require.
@@ -17,3 +18,23 @@ function hello() {
   console.log(`Hello ${this.name}`);
 }
 hello.call({ name: "John" });
+
+
+// `apply()` 
+// Same as `call()`, but arguments are passed as an array.
+// apply() is similar to call(), but it takes an array of arguments instead of a comma-separated list.
+function sum(a, b) {
+  console.log(this.name, a + b);
+}
+sum.apply({ name: "Total:" }, [10, 20]);
+
+
+// `bind()`
+// Returns a new function with fixed `this`.
+// The bind() method creates a new function that, when called, has its this keyword set to the provided value.
+// It allows you to create a new function with a specific context for this, which can be useful in scenarios where you want to ensure that a function is always called with a particular this value.
+function welcome() {
+  console.log("Welcome", this.user);
+}
+const newFn = welcome.bind({ user: "Sarva" });
+newFn()
