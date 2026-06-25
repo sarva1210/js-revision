7a4881cf9282d730e03aeb38a4864e140fa6e064
sarva1210 <sarva.vish77@gmail.com>
2026-06-25 21:08:56 +0530

    anonymous

--- Day14/script.js
+++ Day14/script.js
@@ -1,2 +1,13 @@
 // Class Expression
 // js allows you to define a class using a class expression. A class expression can be named or unnamed. Here's an example of both:
+
+// Anonymous Class Expression
+
+const Person = class {
+  constructor(name) {
+    this.name = name;
+  }
+};
+
+const p = new Person("Josh");
+console.log(p.name);
