f9a424d2cde0b67028290876f3d6b01727565d66
sarva1210 <sarva.vish77@gmail.com>
2026-06-28 12:02:45 +0530

    get User Posts

--- Day17/script.js
+++ Day17/script.js
@@ -17,3 +17,18 @@ function getUser(username, cb){
         cb({id:1, username:"Emma"});
     },1000);
 }
+
+
+// get User Posts
+function getUserPosts(id, cb){
+    console.log("getting user posts....");
+    setTimeout(() => {
+        cb(["hello","good day"]);
+    }, 2000);
+}
+
+getUser("Emma", function(data){
+    getUserPosts(data.id, function(allposts){
+        console.log(data.username, allposts);
+    });
+});
