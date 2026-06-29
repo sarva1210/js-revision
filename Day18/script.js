9127a59e9a9460ff7d6f1b87201c1dfa95b15aa4
sarva1210 <sarva.vish77@gmail.com>
2026-06-29 18:17:15 +0530

    states of promises

--- Day18/script.js
+++ Day18/script.js
@@ -1,2 +1,7 @@
 // promises
 // A promise is an object that represents the eventual completion or failure of an asynchronous operation and its resulting value.
+
+// Three states of a promise:
+// 1. Pending: initial state, neither fulfilled nor rejected.
+// 2. Fulfilled: meaning that the operation completed successfully.
+// 3. Rejected: meaning that the operation failed.
