57698d8020ce11f3b10d296b2bcffd7361f3632c
sarva1210 <sarva.vish77@gmail.com>
2026-06-29 18:09:40 +0530

    promise reject/resolve

--- Day18/script.js
+++ Day18/script.js
@@ -5,3 +5,18 @@
 // 1. Pending: initial state, neither fulfilled nor rejected.
 // 2. Fulfilled: meaning that the operation completed successfully.
 // 3. Rejected: meaning that the operation failed.
+
+
+const promise = new Promise((resolve, reject)=>{
+    setTimeout(() => {
+        resolve();
+    }, 2000);
+});
+
+promise
+    .then(function(){
+        console.log("hey it's resolved");
+    })
+    .catch(function(){
+        console.log("it's rejected");
+});
