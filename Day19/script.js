b8255ca07941285fcda801c1e07425959709a418
sarva1210 <sarva.vish77@gmail.com>
2026-06-30 21:27:59 +0530

    Try-Catch Block

--- Day19/script.js
+++ Day19/script.js
@@ -1,2 +1,16 @@
 // Error Handling
 // it is important to handle errors in your code to prevent unexpected crashes and provide a better user experience.
+
+//  Here are some common techniques for error handling in JavaScript:
+
+// 1. Try-Catch Block: Use try-catch blocks to catch and handle exceptions that may occur during code execution.
+try {
+    let a = 12;
+    console.log(a.name.age);
+} catch(err){
+    console.log(err.name);
+    console.log(err.message);
+    console.log(err.stack);
+}
+
+console.log("heyy");
