619db40024412321270b1e288f3d8b0d1155c59f
sarva1210 <sarva.vish77@gmail.com>
2026-07-01 20:13:18 +0530

    2nd way weather api

--- Day20/script.js
+++ Day20/script.js
@@ -1,5 +1,6 @@
 // Fetch weather data for a given city
 
+// 1st way to fetch data using fetch API
 function getWeather(city) {
     let apikey = `b92e57938eaebc6ded1fcf3a2f5b4bc3`;
 
@@ -11,3 +12,18 @@ function getWeather(city) {
 }
 
 getWeather("Japan");
+
+
+// 2nd way to fetch data using async await
+async function getWeather(city) {
+    let apikey = `b92e57938eaebc6ded1fcf3a2f5b4bc3`;
+
+    let raw = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+    );
+
+    let realdata = await raw.json();
+    console.log(realdata);
+}
+
+getWeather("Paris");
