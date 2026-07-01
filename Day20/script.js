f0d9eded3bd2d6dba567695fcf28518fc829ef48
sarva1210 <sarva.vish77@gmail.com>
2026-07-01 20:17:09 +0530

    email example

--- Day20/script.js
+++ Day20/script.js
@@ -27,3 +27,61 @@ async function getWeather(city) {
 }
 
 getWeather("Paris");
+
+
+// 3rd way to fetch data using async await with error handling
+async function getWeather(city) {
+    try {
+        let apikey = `b92e57938eaebc6ded1fcf3a2f5b4bc3`;
+
+        let raw = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
+        );
+
+        if (!raw.ok) {
+            throw new Error("City not found, find something else");
+        }
+
+        let realdata = await raw.json();
+
+        if (realdata.main.temp < 0) {
+            console.warn(`Too Cold out there... ${realdata.main.temp}°C`);
+        } 
+        else if (realdata.main.temp > 40) {
+            console.warn(`Too Hot out there... ${realdata.main.temp}°C`);
+        } 
+        else {
+            console.log(realdata);
+        }
+
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+getWeather("Las Vegas");
+
+
+// email sending example using promise and async await
+const users = [
+    "josh@gmail.com",
+    "megan@gmail.com",
+    "deniala@gmail.com"
+];
+
+function sendEmail(email) {
+    return new Promise((resolve, reject) => {
+        let time = Math.floor(Math.random() * 5);
+
+        setTimeout(() => {
+            let probability = Math.floor(Math.random() * 10);
+
+            if (probability <= 5) {
+                resolve("Email successfully sent.");
+            } else {
+                reject("Email not sent..");
+            }
+
+        }, time * 300);
+    });
+}
