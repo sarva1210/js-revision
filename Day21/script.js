b2be694ffefd8f9af1ee7e7ac0c6be7bd22dfd9c
sarva1210 <sarva.vish77@gmail.com>
2026-07-02 18:23:54 +0530

    Debouncing

--- Day21/script.js
+++ Day21/script.js
@@ -1,1 +1,18 @@
 // debouncing & throttling
+
+// Debouncing
+// Delays the execution of a function until after a certain amount of time has passed since the last time it was called.
+
+function debounce(fn, delay){
+    let timer;
+    return function(){
+        clearTimeout(timer);
+        timer = setTimeout(fn, delay);
+    }
+}
+
+document.querySelector('#search')
+.addEventListener("input", debounce(function(){
+    console.log("working");
+},600)
+);  
