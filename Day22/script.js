bf5c63d5ba1b26f10e31b3bb7cc78d1218fe718b
sarva1210 <sarva.vish77@gmail.com>
2026-07-03 17:02:17 +0530

    que 1

--- Day22/script.js
+++ Day22/script.js
@@ -1,1 +1,19 @@
 // OOPS Thinking with Objects
+
+// 1. Create an object called laptop that contains brand, price, and a start method that prints “Laptop started”. 
+// 2. Add one more method to the same object that increases the price by 10 percent.
+const laptop = {
+  brand: "Samsung",
+  price: 67000,
+  start: function () {
+    console.log("Laptop started");
+  }
+};
+laptop.start();
+
+laptop.increasePrice = function () {
+  this.price = this.price + (this.price * 10) / 100;
+};
+console.log(laptop.price);
+laptop.increasePrice();
+console.log(laptop.price);
