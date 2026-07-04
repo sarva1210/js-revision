30cad6ba3f4ebf17816ff5c565386b5ebb0e96d8
sarva1210 <sarva.vish77@gmail.com>
2026-07-04 19:04:13 +0530

    que1

--- Day23/script.js
+++ Day23/script.js
@@ -1,1 +1,13 @@
 // questions
+
+// 11. Create an object named profile with a property username and a method printName that logs this.username. 
+// 12. Call the method normally and observe the output.
+const profile = {
+  username: "Sarva",
+  printName() {
+    console.log(this.username);
+  }
+};
+profile.printName(); 
+const fn = profile.printName;
+fn(); // undefined
