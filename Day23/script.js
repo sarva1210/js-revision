b327258eb71069228c5bf2431a0f99aed2063a2d
sarva1210 <sarva.vish77@gmail.com>
2026-07-04 19:05:17 +0530

    modified

--- Day23/script.js
+++ Day23/script.js
@@ -2,6 +2,8 @@
 
 // 11. Create an object named profile with a property username and a method printName that logs this.username. 
 // 12. Call the method normally and observe the output.
+// 13. Store the method in a separate variable and call it
+// 14. Modify the code so that this works correctly again.
 const profile = {
   username: "Sarva",
   printName() {
@@ -11,3 +13,6 @@ const profile = {
 profile.printName(); 
 const fn = profile.printName;
 fn(); // undefined
+
+const fixed = profile.printName.bind(profile);
+fixed(); 
