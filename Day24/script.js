c028aaec60f5e38492711ae6fe92b92d701b7f74
sarva1210 <sarva.vish77@gmail.com>
2026-07-05 21:48:47 +0530

    que 1

--- Day24/script.js
+++ Day24/script.js
@@ -1,2 +1,15 @@
 // call Method Practice
 // call is a method that allows you to invoke a function with a specific this value and arguments provided individually.
+
+// 19. Create a function showBrand that prints this.brand. 
+// 20. Create two different objects with brand values. 
+// 21. Use call to execute showBrand for both objects
+function showBrand() {
+  console.log(this.brand);
+}
+
+const a = { brand: "Samsung" };
+const d = { brand: "Apple" };
+
+showBrand.call(a);
+showBrand.call(d);
