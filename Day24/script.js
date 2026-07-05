f94d0e0bf7ff6820b98e55becf564f82ff58a7d9
sarva1210 <sarva.vish77@gmail.com>
2026-07-05 21:51:40 +0530

    que 2

--- Day24/script.js
+++ Day24/script.js
@@ -17,3 +17,14 @@ showBrand.call(d);
 
 // apply Method Practice
 // apply is a method that allows you to invoke a func with a specific this value and arguments provided as an array (or an array like obj).
+
+// 23. Create a function introduce that accepts two arguments: city and role, and prints name, city, and role using this.name. 
+// 24. Create an object with a name property. 
+// 25. Use apply to call introduce using the object and an array of arguments
+function introduce(city, role) {
+  console.log(this.name, city, role);
+}
+
+const person = { name: "Josh" };
+
+introduce.apply(person, ["Banglore", "Developer"]);
