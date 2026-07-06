f7978c77b5c2aeac3d8f89050faa036b759a1b9f
sarva1210 <sarva.vish77@gmail.com>
2026-07-06 20:31:29 +0530

    que

--- Day25/script.js
+++ Day25/script.js
@@ -1,3 +1,7 @@
 // bind Method Practice
 // bind() method creates a new function that, when called,
 // has its this keyword set to the provided value, with a given sequence of arguments preceding any provided when the new function is called.
+
+// 27. Create a function greet that prints “Hello” followed by this.name. 
+// 28. Bind this function to an object and store the returned function in a variable. 
+// 29. Call the bound function later and observe the output.
