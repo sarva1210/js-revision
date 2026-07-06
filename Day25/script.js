6fc1b34babdfb912408f39ad49087388e24cb814
sarva1210 <sarva.vish77@gmail.com>
2026-07-06 20:28:33 +0530

    def

--- Day25/script.js
+++ Day25/script.js
@@ -1,1 +1,3 @@
 // bind Method Practice
+// bind() method creates a new function that, when called,
+// has its this keyword set to the provided value, with a given sequence of arguments preceding any provided when the new function is called.
