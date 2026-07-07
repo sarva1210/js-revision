2e55f81ee5b40a15c02c6ab1cc1d50136b4c18e3
sarva1210 <sarva.vish77@gmail.com>
2026-07-07 18:33:43 +0530

    js

--- Day26/script.js
+++ Day26/script.js
@@ -1,15 +1,15 @@
-var inc = document.getElementById("inc");
-var dec = document.getElementById("dec");
-var h2 = document.querySelector("h2");
+var inc = document.querySelector('#inc')
+var dec = document.querySelector('#dec')
+var h2 = document.querySelector('h2')
 
-var count = 0;
+var count = 0
 
-inc.addEventListener("click", function () {
-    count++;
-    h2.innerHTML = count;
-});
+inc.addEventListener('click',function(){
+    count++
+    h2.innerHTML = count
+})
 
-dec.addEventListener("click", function () {
-    count--;
-    h2.innerHTML = count;
-});
+dec.addEventListener('click',function(){
+    count--
+    h2.innerHTML = count
+})
