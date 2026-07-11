1f21ee42932ec9c382342018b6211ccdf8a83f6c
sarva1210 <sarva.vish77@gmail.com>
2026-07-11 16:51:51 +0530

    download animation

---
+++ Day29/script.js
@@ -0,0 +1,22 @@
+var grow = 0     
+var btn = document.querySelector('button')
+var h2 = document.querySelector('h2')
+var inner = document.querySelector('.inner')
+
+btn.addEventListener('click',function(){
+
+    btn.style.pointerEvents = 'none'
+    var num = 50 + Math.floor(Math.random()*50)
+    console.log('Your file will be downloaded in',num/10,'seconds');
+    var int = setInterval(()=>{
+        grow++
+        h2.innerHTML = grow+'%'
+        inner.style.width = grow+'%'
+    },50);
+
+    setTimeout(()=>{
+        clearInterval(int)
+        btn.innerHTML = 'Downloaded'
+        btn.style.opacity = 0.5;
+    },5000);
+})
