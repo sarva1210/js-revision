b03a3d2c1c9c21c1c6810839d3696524d4737239
sarva1210 <sarva.vish77@gmail.com>
2026-07-26 12:22:22 +0530

    find common elements

--- Day38/script.js
+++ Day38/script.js
@@ -151,3 +151,11 @@ function bubbleSort(arr){
 }
 
 console.log(bubbleSort([7,3,1,9,5]));
+
+
+// find common elements in two arrays
+function common(arr1, arr2) {
+    return arr1.filter(item => arr2.includes(item));
+}
+
+console.log(common([1,2,3,4],[3,4,5,6]));
