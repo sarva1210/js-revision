ca2bb671867d4382b131571c7b128350390913be
sarva1210 <sarva.vish77@gmail.com>
2026-07-27 12:15:38 +0530

    merge 2 sorted array

--- Day38/script.js
+++ Day38/script.js
@@ -208,3 +208,19 @@ findPairs([2,4,3,5,7,8], 10);
 
 
 // Merge Two Sorted Arrays
+function mergeSorted(arr1, arr2) {
+    let i = 0;
+    let j = 0;
+    let result = [];
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i++]);
+        } else {
+            result.push(arr2[j++]);
+        }
+    }
+    return result.concat(arr1.slice(i), arr2.slice(j));
+}
+
+console.log(mergeSorted([1,3,5],[2,4,6]));
