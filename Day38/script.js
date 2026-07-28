6320a53a368fdb8156393fd398e48bb6b5f66c39
sarva1210 <sarva.vish77@gmail.com>
2026-07-28 12:29:02 +0530

    check if 2 arr are equal

--- Day38/script.js
+++ Day38/script.js
@@ -236,3 +236,21 @@ console.log(maxProduct([3, 5, 2, 8, 7]));
 
 
 // Check if Two Arrays are Equal
+function arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length)
+        return false;
+
+    arr1.sort((a,b)=>a-b);
+    arr2.sort((a,b)=>a-b);
+
+    for(let i=0;i<arr1.length;i++){
+
+        if(arr1[i]!==arr2[i])
+            return false;
+
+    }
+
+    return true;
+}
+
+console.log(arraysEqual([1,2,3],[3,2,1]));
