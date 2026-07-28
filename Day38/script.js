9b27f5f3cabd01a79910b6bc5e716f5364b1fa01
sarva1210 <sarva.vish77@gmail.com>
2026-07-28 12:40:16 +0530

    find largest element

--- Day38/script.js
+++ Day38/script.js
@@ -264,3 +264,18 @@ console.log(minProduct([3, 5, 2, 8, 7]));
 
 
 // find the largest element in array
+function findLargest(arr) {
+    if (arr.length === 0) return null;
+
+    let largest = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    return largest;
+}
+
+console.log(findLargest([12, 45, 67, 23, 89, 34]));
