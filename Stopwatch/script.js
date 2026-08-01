94a4cdbbc64b1b34ab45af290508e1959589cf68
sarva1210 <sarva.vish77@gmail.com>
2026-08-01 14:46:17 +0530

    js

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -29,7 +29,6 @@ let fastest=null;
 
 // Format time
 function formatTime(ms){
-
     const hrs=Math.floor(ms/3600000);
 
     const mins=Math.floor((ms%3600000)/60000);
@@ -37,14 +36,11 @@ function formatTime(ms){
     const secs=Math.floor((ms%60000)/1000);
 
     return`${String(hrs).padStart(2,"0")}:${String(mins).padStart(2,"0")}:${String(secs).padStart(2,"0")}`;
-
 }
 
 // Update display
 function updateDisplay(){
-
     display.textContent=formatTime(totalMilliseconds);
-
 }
 
 // Show toast
