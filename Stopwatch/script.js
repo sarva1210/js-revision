a9d18bad3eb77bb8cebe1880fa1ea8b01abf67d4
sarva1210 <sarva.vish77@gmail.com>
2026-08-31 17:20:04 +0530

    saved vol

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -796,4 +796,25 @@ function loadSettings(){
             savedSound
         );
     }
+
+    const savedVolume =
+        localStorage.getItem(
+            "focusflow-volume"
+        );
+
+    if(savedVolume !== null){
+
+        volumeSlider.value =
+            savedVolume;
+
+        audioPlayer.volume =
+            Number(savedVolume);
+
+    }else{
+
+        volumeSlider.value = 0.5;
+
+        audioPlayer.volume = 0.5;
+
+    }
 }
